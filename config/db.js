8d74f632955a5ef6ba041021c1e6704d8820ff79
pratheeshraj <rajeshnsk5@gmail.com>
2023-02-28 00:25:17 +0530

    f

---
+++ config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const connectDB = async ()=>{
+    mongoose.set('strictQuery', false);
+   await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("Database connected successfully.")).catch((err) => {console.log(err);
+    })
+}
+module.exports = connectDB;
+
